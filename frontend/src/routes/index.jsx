ec8b96a1a6c8700dd24bb7966b766701eadd4124
Lahmar22 <lahmarzakariae22@gmail.com>
2026-03-31 09:15:42 +0100

    feat : update route for add the link

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login.jsx";
 import Register from "../pages/Register.jsx";
+import DashboardAdmin from "../pages/admin/Dashboard.jsx";
+import DashboardEmploye from "../pages/employe/Dashboard.jsx";
+import DashboardClient from "../pages/client/Dashboard.jsx";
+import ClientProducts from "../pages/client/Products.jsx";
+import ClientAbout from "../pages/client/About.jsx";
 
 export default function Router() {
     return (
@@ -10,7 +15,11 @@ export default function Router() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register/>} />
-
+                <Route path="/admin/dashboard" element={<DashboardAdmin />} />
+                <Route path="/employe/dashboard" element={<DashboardEmploye />} />
+                <Route path="/client/dashboard" element={<DashboardClient />} />
+                <Route path="/client/products" element={<ClientProducts />} />
+                <Route path="/client/about" element={<ClientAbout />} />
             </Routes>
         </BrowserRouter>
     );
